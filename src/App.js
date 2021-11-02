f6199400e915c327035f7f771a819afe3f79f867
unknown <ah53622@gmail.com>
2021-11-02 10:10:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import initializeAuthentication from './Firebase/firebase.init';
+import { useState } from 'react'
+
+initializeAuthentication();
+const googleProvider = new GoogleAuthProvider();
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+ const auth = getAuth();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
+  }
+
+  const handleEmailChange = e => {
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordChange = e => {
+    setPassword(e.target.value);
+  }
+
+  const handleResistration = e => {
+    console.log(email, password);
+   e.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-5">
+      <form onSubmit={handleResistration}>
+        <h3 className="text-primary">Please Resister</h3>
+  <div className="row mb-3">
+    <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+    <div className="col-sm-10">
+      <input onBlur={handleEmailChange} type="email" className="form-control" id="inputEmail3" />
+    </div>
+  </div>
+  <div className="row mb-3">
+    <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+    <div className="col-sm-10">
+      <input onBlur={handlePasswordChange} type="password" className="form-control" id="inputPassword3" />
+    </div>
+  </div>
+  <div className="row mb-3">
+    <div className="col-sm-10 offset-sm-2">
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox" id="gridCheck1" />
+        <label className="form-check-label" htmlFor="gridCheck1">
+          Example checkbox
+        </label>
+      </div>
+    </div>
+  </div>
+  <button type="submit" className="btn btn-primary">Resister</button>
+</form>
+      <br /><br /><br />
+      <div>---------------------------------</div>
+      <br /><br /><br />
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
     </div>
   );
 }
---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDGwKan1SjuckCldmFMi5t11ECZajzP9d4",
+  
+    authDomain: "react-firebase-authentic-332bf.firebaseapp.com",
+  
+    projectId: "react-firebase-authentic-332bf",
+  
+    storageBucket: "react-firebase-authentic-332bf.appspot.com",
+  
+    messagingSenderId: "735028757183",
+  
+    appId: "1:735028757183:web:3f060d80230260d0c67c0a"
+  
+  };
+  export default firebaseConfig;
